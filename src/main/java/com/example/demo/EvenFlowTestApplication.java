da05835d35889ef69fb73b0052a14c035bdbc891
PRATHAM ~ <prathamcomeslast@gmail.com>
2026-02-20 03:21:14 +0530

    Update title in HTML to reflect deployment message

--- src/main/java/com/example/demo/EvenFlowTestApplication.java
+++ src/main/java/com/example/demo/EvenFlowTestApplication.java
@@ -24,7 +24,7 @@ public class EvenFlowTestApplication {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>EvenFlow | Deployment Success</title>
+                <title>EvenFlow | Deployment ho gaya!!!</title>
                 <style>
                     * {
                         margin: 0;
